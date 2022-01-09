3381b082e3a9fe65ba96c251c1ce3cbf0341e788
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-01-09 09:46:11 +0100

    rendring with map function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -4,14 +4,16 @@ class Counter extends React.Component {
   state = {
     count: 1,
     // imageUrl: 'https://picsum.photos/100'
+    tags: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5']
     
   };
   
   render() { 
     return (
-      <div>
+      <div className="content">
         {/* <img className="img m-4" src={this.state.imageUrl} alt="picsum" /> */}
         <span className={this.getBadgeClasses()} style={{fontSize: 17}}>{this.formatCount()}</span>
+        <ul className='tags' style={{listStyle: 'none'}}>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
      
