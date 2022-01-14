a2203d9b37ec50b56328d5f5e202653a4a51a55a
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-01-14 11:41:08 +0100

    moved the state to the highest component so that other lower components can acess it

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
 import './App.css';
 import NavBar from "./components/NavBar";
 import Counters from "./components/counters";
+import { Component } from "react";
 
-function App() {
+class App extends Component {
+  state = {
+    counters: [
+      {id: 1, value: 0},
+      {id: 2, value: 4},
+      {id: 3, value: 0},
+      {id: 4, value: 0}
+    ]
+  }
+  
+  handleIncrement = (counter) => { 
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter)
+    counters[index] = { ...counter }
+    counters[index].value ++
+    this.setState({counters})
+  }
+  //  handleDecrement = (counter) => {
+  //   const dcounters = [...this.state.counters];
+  //   const index = dcounters.indexOf(counter)
+  //   dcounters[index] = { ...counter }
+  //   dcounters[index].value -=
+  //   this.setState({dcounters})
+  // }
+
+  //RULL OF THUMB:ONLY THE COMPONENTS THAT OWNS A STATE CAN MODIFY IT
+  //HENCE WE RAISED THE EVENT HANDLE HERE PASS IT AS PROP TO counter.js
+  handleDelete = (id) => {
+    const data = this.state.counters.filter(item => item.id !== id)
+    this.setState({counters:data}) 
+    
+  }
+
+  handleReset = () => {
+    const counters = this.state.counters.map(i => {
+      i.value = 0;
+      return i;
+    });
+    this.setState({counters})
+  }
+  render() {
   return (
     <div className="App">
-      <NavBar />
+      <NavBar totalCounters={this.state.counters.filter(c => c.value > 0).length}/>
       <main className="container">
-     <Counters />
+      <Counters 
+     counters={this.state.counters} 
+     onReset={this.handleReset} 
+     onIncrememnt={this.handleIncrement} 
+     onDelete={this.handleDelete}/>
       </main>
     </div>
   );
+}
 }
 
 export default App;
