f7e32b8990b6cd6c48b6ee8d8ef9fb14c62915da
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2022-01-14 10:30:42 +0100

    add navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import NavBar from "./components/NavBar";
 import Counters from "./components/counters";
 
 function App() {
   return (
     <div className="App">
+      <NavBar />
+      <main className="container">
      <Counters />
+      </main>
     </div>
   );
 }
